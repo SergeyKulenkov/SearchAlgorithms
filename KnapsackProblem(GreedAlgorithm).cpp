662f5b96a58bd8d925e5dce85848fe1829f2eb30
SergeyKulenkov <60221788+SergeyKulenkov@users.noreply.github.com>
2020-01-24 11:26:47 +0300

    gitadd

---
+++ KnapsackProblem(GreedAlgorithm).cpp
@@ -0,0 +1,83 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "fstream"
+#include "stdlib.h"
+
+using namespace std;
+
+const int N = 15;
+
+void OutAll(int Q, int sum)
+{
+	cout  << "total price" << Q << endl;
+	cout << "total weight" << sum << endl << endl;
+}
+
+int main()
+{
+	int currentWeight = 0, summaryWeight = 0, Solution[N], maxWeight = 0, maxPrice = 0, Quality = 0, Price[N], Weight[N];
+	int index, price, weight;
+	ifstream text("C:\\Users\\Elen\\Desktop\\visual\\file9.txt");
+
+	while (text >> index >> price >> weight)
+	{
+		Price[index - 1] = price;
+		Weight[index - 1] = weight;
+		summaryWeight += Weight[index - 1];
+		cout << index << " " << price << " " << weight << " " << endl;
+	}
+
+	cout << "enter maxWeight";
+	cin >> maxWeight;
+	while (maxWeight > summaryWeight)
+	{
+		cout << "maxWeight is too big" << endl;
+		cout << "enter maxWeight";
+		cin >> maxWeight;
+	}
+	
+	for (int i = 0; i < N; i++)
+	{
+		Solution[i] = 0;
+	}
+
+	for (int j = 0; j < N; j++)
+	{
+		cout << "step number - " << j + 1 << endl;
+		int e = 0;
+		for (int i = 0; i < N; i++)
+		{
+			if (maxPrice < Price[i])
+			{
+				e = i;
+				maxPrice = Price[i];
+			}
+		}
+		Price[e] = 0;
+
+		if (currentWeight + Weight[e] <= maxWeight)
+		{
+			Quality += maxPrice;
+			currentWeight += Weight[e];
+			Solution[e] = 1;
+
+			cout << "number of the element - " << e + 1 << endl;
+			cout << "price of this element - " << maxPrice << endl;
+			OutAll(Quality, currentWeight);
+		}
+
+		maxPrice = 0;
+	}
+
+	cout << endl << endl;
+	OutAll(Quality, currentWeight);
+	for (int i = 0; i < N; i++)
+	{
+		cout << Solution[i] << " ";
+	}
+	cout << endl << endl;
+
+	return 0;
+}
---
+++ NearestNeighborAlgorithm.cpp
@@ -0,0 +1,68 @@
+﻿// metod 6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "stdlib.h"
+#include "time.h"
+
+using namespace std;
+
+const int n = 15;
+
+int main()
+{
+	int Distance[n][n], minimalDistance = 0, i = 0, Solution[n], Indexes[n], elements = n, summaryDistance = 0;
+	srand(time(0));
+	int r = rand() % n, r2 = n;
+
+	for (int j = 0; j < n; j++)
+	{
+		Solution[j] = 0;
+		Indexes[j] = j;
+		for (int k = 0; k < n; k++)
+		{
+			Distance[j][k] = rand() % r2 + 1;
+			if (k == j) Distance[j][k] = 0;
+			cout << Distance[j][k] << " ";
+		}
+		cout << endl;
+	}
+	minimalDistance = 20;
+
+	Solution[i] = Indexes[r];
+	cout << "first city - " << Solution[i] + 1 << endl << endl;
+	Indexes[r] = -1;
+	elements -= 1;
+	i++;
+
+	while(elements != 0)
+	{
+		int el = 0;
+		for (int j = 0; j < n; j++)
+		{
+			if (minimalDistance > Distance[Solution[i-1]][j] && Indexes[j] != -1)
+			{
+				el = j;
+				minimalDistance = Distance[Solution[i - 1]][j];
+			}
+		}
+		Solution[i] = Indexes[el];
+		Indexes[el] = -1;
+		summaryDistance += minimalDistance;
+		cout << "step - " << i << endl;
+		cout << "elements in X - " << elements << endl;
+		cout << "distance - " << minimalDistance << endl;
+		cout << "number - " << Solution[i] + 1<< endl;
+		cout << "total distance " << summaryDistance << endl;
+		elements -= 1;
+		i++;
+		minimalDistance = r2;
+	}
+
+	cout << endl << endl;
+	summaryDistance += Distance[Solution[n-1]][Solution[0]];
+	cout << "total distance - " << summaryDistance << endl;
+	for (int j = 0; j < n; j++) cout << Solution[j] + 1	<< " ";
+
+	return 0;
+}
